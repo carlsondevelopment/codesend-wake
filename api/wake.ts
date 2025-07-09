632c07929b6a9b6a0c605e7ec497e4f7212ac2df
CodeShare <j.joshcarlson@gmail.com>
2025-07-09 18:20:21 -0400

    Initial commit: Twilio wake function

---
+++ api/wake.ts
@@ -0,0 +1,29 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+import twilio from 'twilio'
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID!
+const authToken = process.env.TWILIO_AUTH_TOKEN!
+const twilioNumber = process.env.TWILIO_PHONE_NUMBER!
+
+const client = twilio(accountSid, authToken)
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const number = req.query.number as string
+
+  if (!number) {
+    return res.status(400).send('Missing phone number')
+  }
+
+  try {
+    await client.calls.create({
+      to: number,
+      from: twilioNumber,
+      twiml: '<Response><Pause length="1"/></Response>',
+    })
+    res.status(200).send('Call placed')
+  } catch (err) {
+    console.error('Twilio error:', err)
+    res.status(500).send('Failed to call')
+  }
+}
+
